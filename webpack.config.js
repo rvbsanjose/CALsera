a2e45a78d4fc904f88e8fa5d0d364eb38576e353
Richard VanBreemen <rvbsanjose@me.com>
2017-02-24 12:49:40 -0800

    CAL-001 Setup build process for CALsera

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const pkg = require('./package.json'),
+    path = require('path'),
+    webpack = require('webpack'),
+    HtmlwebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+    dist: path.join(__dirname, 'dist'),
+    src: path.join(__dirname, 'src'),
+    vendor: Object.keys(pkg.dependencies)
+};
+
+module.exports = {
+    devServer: {
+        hot: true,
+        inline: true
+    },
+    devtool: 'source-map',
+    entry: {
+        app: PATHS.src,
+        vendor: PATHS.vendor
+    },
+    resolve: {
+        extensions: [ '.js', '.jsx' ]
+    },
+    output: {
+        path: PATHS.dist,
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loaders: [ 'babel-loader' ],
+                include: PATHS.src
+            },
+            {
+                test: /\.scss$/,
+                loaders: [ 'style', 'css?sourceMap', 'sass?sourceMap' ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+
+        new HtmlwebpackPlugin({
+            title: 'CALsera',
+            inject: false,
+            template: './src/index.html'
+        }),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            names: [ 'vendor' ]
+        })
+    ]
+};
